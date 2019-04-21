81820679e592d264f1e574ceaf894e6f20e0b79c
Mauro <maurogch@gmail.com>
2019-04-21 17:24:24 -0300

    Finished

--- Guia 2 - Cursores.sql
+++ Guia 2 - Cursores.sql
@@ -80,6 +80,8 @@ begin
             leave leer;
         end if;
     end loop leer;
+
+    close leerPartidos;
 end;
 
 call resultadosPartidos();
@@ -103,18 +105,19 @@ begin
     declare vPuntosVisitante int;
     declare vFin int default 0;
 
-    declare leerPartidos cursor for select p.idPartido, 
-                                        ifnull((select sum(puntos)
-                                        from jugadores_x_equipo_x_partido jxe
-                                        where jxe.idPartido = el.idEquipo),0),
-                                        ifnull((select sum(puntos)
-                                        from jugadores_x_equipo_x_partido jxe
-                                        where jxe.idPartido = ev.idEquipo),0)
-                                    from partidos p 
-                                    inner join equipos el
-                                    on p.idEquipoLocal = el.idEquipo
-                                    inner join equipos ev
-                                    on p.idEquipoVisitante = ev.idEquipo;
+    declare leerPartidos cursor for 
+        select p.idPartido, 
+            ifnull((select sum(puntos)
+            from jugadores_x_equipo_x_partido jxe
+            where jxe.idPartido = el.idEquipo),0),
+            ifnull((select sum(puntos)
+            from jugadores_x_equipo_x_partido jxe
+            where jxe.idPartido = ev.idEquipo),0)
+        from partidos p 
+        inner join equipos el
+        on p.idEquipoLocal = el.idEquipo
+        inner join equipos ev
+        on p.idEquipoVisitante = ev.idEquipo;
     
     declare continue handler for not found set vFin = 1;
     open leerPartidos;
@@ -131,6 +134,8 @@ begin
             puntosEquipoVisitante = vPuntosVisitante
         where idPartido = vIdPartido;
     end loop leer;
+
+    close leerPartidos;
 end;
 
 select * from partidos;
@@ -154,11 +159,12 @@ begin
     declare vPromedio float;
     declare vFin int default 0;
 
-    declare leerJugadores cursor for select j.idJugador, avg(puntos)
-                                    from jugadores j
-                                    inner join jugadores_x_equipo_x_partido jxe
-                                    on j.idJugador = jxe.idJugador
-                                    group by j.idJugador;
+    declare leerJugadores cursor for 
+        select j.idJugador, avg(puntos)
+        from jugadores j
+        inner join jugadores_x_equipo_x_partido jxe
+        on j.idJugador = jxe.idJugador
+        group by j.idJugador;
     
     declare continue handler for not found set vFin = 1;
     open leerJugadores;
@@ -174,6 +180,8 @@ begin
         set promedio = vPromedio
         where idJugador = vIdJugador;
     end loop leer;
+
+    close leerJugadores;
 end;
 
 select * from jugadores;
@@ -182,6 +190,217 @@ call agregarPromedioJugadores();
 
 select * from jugadores;
 
-/*5 - Generar una tabla con el nombre records_jugadores, donde se guarde la máxima
+/*5 - Generar una tabla con el nombre recordsJugadores, donde se guarde la máxima
 cantidad de puntos hecha por un jugador y en que partido lo hizo.*/
+
+-- Tabla tiene que ser primary foreign key? Como se guarda solo el mejor record no la hice
+create table recordsJugadores(
+    idRecord int auto_increment,
+    idJugador int not null unique,
+    idPartido int not null,
+    record int not null,
+    primary key (idRecord),
+    foreign key (idJugador) references jugadores (idJugador),
+    foreign key (idPartido) references partidos (idPartido)
+);
+
+/*Append: El siguiente query no sirve ya que agrupa y devuelve el primer row del todos los max
+en este caso el partido no corresponde al max del jugador
+
+select idJugador, idPartido, max(puntos)
+from jugadores_x_equipo_x_partido
+group by idJugador
+
+--Para resolver eso se usa una query especial (solo es necesario cuando se necesita más que 
+  el max y el valor a group by, en este caso el partido)
+--Se llama en el primer query todos los valores normalmente, luego un inner join con un
+  sub query a la misma tabla haciendo el max y el valor a agrupar, y en las condiciendoes
+  del on se igualan ambos valores (importante poner alias al max)
+
+select jxe.idJugador, jxe.idPartido, jxe.puntos
+from jugadores_x_equipo_x_partido jxe
+inner join (
+    select idJugador, max(puntos) as puntos
+    from jugadores_x_equipo_x_partido
+    group by idJugador
+) b on jxe.idJugador = b.idJugador and jxe.puntos = b.puntos
+group by jxe.idJugador
+
+--Por ultimo el group by final es por si hay resultados repetidos, ejemplo en dos partidos un jugador
+  hizo 10 puntos
+*/
+
+drop procedure agregarRecordJugadores;
+create procedure agregarRecordJugadores()
+begin
+    declare vIdJugador int;
+    declare vIdPartido int;
+    declare vPuntos int;
+    declare vFin int default 0;
+
+    declare leerJugadores cursor for
+        select jxe.idJugador, jxe.idPartido, jxe.puntos
+        from jugadores_x_equipo_x_partido jxe
+        inner join (
+            select idJugador, max(puntos) as puntos
+            from jugadores_x_equipo_x_partido
+            group by idJugador
+        ) b on jxe.idJugador = b.idJugador and jxe.puntos = b.puntos
+        group by jxe.idJugador;
+
+    declare continue handler for not found set vFin = 1;
+    open leerJugadores;
+
+    leer: loop
+        fetch leerJugadores into vIdJugador, vIdPartido, vPuntos;
+
+        if vFin = 1 then
+            leave leer;
+        end if;
+
+        insert into recordsJugadores (idJugador, idPartido, record)
+        values (vIdJugador, vIdPartido, vPuntos)
+        on duplicate key update idPartido = values(idPartido), record = values(record);
+    end loop leer;
+
+    close leerJugadores;
+end;
+
+/*Notar uso de "on duplicate key", en caso de que ya exista un record para ese jugador*/
+
+call agregarRecordJugadores();
+
+select * from recordsJugadores;
+
+/*6 - Dado un mes y año pasado por parámetro, generar una tabla llamada
+estadisticas_<mes>_<año>, donde se guarden los siguientes campos :
+
+apellido y nombre
+equipo
+cantidad_partidos
+puntos
+rebotes
+asistencias
+
+Para cada uno de los partidos y jugadores debe guardarse si esa información ya fue
+tomada en cuenta para el calculo y en caso de que no actualizar los valores de la tabla.*/
+
+/*Query obsoleta que devuelve los valores en una sola tabla, sirve para verificar
+que los valores del stored procedure sean correctos
+
+select jxe.idJugador, concat(j.apellido,' ', j.nombre), count(jxe.idJugador), ifnull(sum(puntos),0), ifnull(sum(rebotes),0), ifnull(sum(asistencias),0)
+from jugadores_x_equipo_x_partido jxe
+right join jugadores j
+on jxe.idJugador = j.idJugador
+group by j.idJugador
+
+*/
+
+/*Append: note te use of concat and stmt for creating tables with variable names*/
+
+/*Jugadores sin equipo (que fueron puestos solo para testear) no son llamados, si se quisiera que se llamen
+cambiar "inner join equipos e" a "left join equipos e"*/
+
+alter table jugadores_x_equipo_x_partido
+add column estadistica tinyint(1) default 0;
+
+drop procedure estadisticasMesAno;
+create procedure estadisticasMesAno(in vMes int, in vAno int)
+begin
+    declare vIdJugador int;
+    declare vIdPartido int;
+    declare vNombre varchar(255);
+    declare vEquipo varchar(255);
+    declare vPuntos int;
+    declare vRebotes int;
+    declare vAsistencias int;
+    declare vFin int default 0;
+    
+    declare leerJugadores cursor for
+        select j.idJugador, jxe.idPartido, concat(j.apellido,' ', j.nombre), e.equipo, ifnull(jxe.puntos,0), ifnull(jxe.rebotes,0), ifnull(jxe.asistencias,0)
+        from jugadores_x_equipo_x_partido jxe
+        right join jugadores j
+        on jxe.idJugador = j.idJugador
+        inner join equipos e
+        on j.idEquipo = e.idEquipo
+        order by j.idJugador;
+
+    declare continue handler for not found set vFin = 1;
+
+    set @sql_text = concat('create table estadisticas_',vMes,'_',vAno,'
+    (
+        idEstadisticas int auto_increment,
+        idJugador int not null unique,
+        apellidoNombre varchar(255) not null,
+        equipo varchar(255) not null,
+        cantidadPartidos int,
+        puntos int,
+        rebotes int,
+        asistencias int,
+        primary key (idEstadisticas),
+        foreign key (idJugador) references jugadores (idJugador)
+    )');
+
+    PREPARE stmt from @sql_text;
+    EXECUTE stmt; 
+    DEALLOCATE PREPARE stmt;
+
+    
+    open leerJugadores;
+
+    leer: loop
+        fetch leerJugadores into vIdJugador, vIdPartido, vNombre, vEquipo, vPuntos, vRebotes, vAsistencias;
+
+        if (vFin = 1) then
+            leave leer;
+        end if;
+
+        set @sql_text = concat('
+            insert into estadisticas_',vMes,'_',vAno,' (idJugador, apellidoNombre, equipo, cantidadPartidos, puntos, rebotes, asistencias) 
+            values(',vIdJugador,',"',vNombre,'","',vEquipo,'",1,',vPuntos,',',vRebotes,',',vAsistencias,') 
+            on duplicate key update cantidadPartidos = cantidadPartidos + 1, puntos = puntos + values(puntos), 
+                rebotes = rebotes + values(rebotes), asistencias = asistencias + values(asistencias)
+            ');
+        
+        PREPARE stmt from @sql_text;
+        EXECUTE stmt; 
+        DEALLOCATE PREPARE stmt;
+
+        update jugadores_x_equipo_x_partido
+        set estadistica = 1
+        where idJugador = vIdJugador
+        and idPartido = vIdPartido;
+
+    end loop leer;
+
+    close leerJugadores;
+end;
+
+call estadisticasMesAno(04,2019);
+
+select * from estadisticas_4_2019;
+
+select * from jugadores_x_equipo_x_partido;
+
+/*7 - Agregar al ejercicio 6 un campo que indique la cantidad de triple dobles que hizo cada
+jugador.*/
+
+-- se crean variables count = 0 y vTripleDouble = 0
+-- luego del fetch dentro del loop
+
+if (vPuntos > 9) then
+    count = count + 1;
+end if;
+if (vRebotes > 9) then
+    count = count + 1;
+end if;
+
+-- lo mismo con los 5 tipos de valores (que encima hay que agregar 2 que no fueron tomados en cuenta en el anterior)
+-- si count es 3 o mayor, se suma un punto a los triple-double
+
+if(count >= 3) then
+    vTripleDouble = 1;
+end if;
+
+-- y se suma en el insert y update
 
