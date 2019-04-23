99572a5dbc58fcfb959f9feb1ace51490a42706b
Mauro <maurogch@gmail.com>
2019-04-23 17:14:45 -0300

    4 to 8

--- Guia 3 - Transactions.sql
+++ Guia 3 - Transactions.sql
@@ -49,3 +49,47 @@ values (1,2,now());
 select * from partidos
 where month(fecha) = 04;
 
+/*4 - Ingresar dos equipos con sus jugadores e ingresar un partido con dichos equipos ingresados, todo 
+en la misma transacción.*/
+
+start transaction;
+
+insert into equipos (equipo)
+values ("Equipo trans1");
+
+set @idEquipo = last_insert_id();
+
+insert into jugadores (insert into jugadores (idEquipo, nombre, apellido, fechaNacimiento)
+values (@idEquipo, "Mega", "Jugador", "1987-05-20")
+
+insert into equipos (equipo)
+values ("Equipo trans1");
+
+set @idEquipo = last_insert_id();
+
+insert into jugadores (insert into jugadores (idEquipo, nombre, apellido, fechaNacimiento)
+values (@idEquipo, "Hyper Mega", "Jugador", "1987-05-21");
+
+commit;
+
+/*5 - Ejecutar la transacción del punto 1 con datos erróneos y verificar si ingreso los datos.*/
+
+/*Rollback probado, usar workbench*/
+
+/*6 - Ejecutar la transacción del punto 2 con datos erróneos y mostrar por pantalla un mensaje que 
+indique si el error fue provocado en la inserción del equipo o del jugador.*/
+
+#???
+
+/*7 - Ingresar un quipo, modificarle el nombre y dejarlo en el estado original sin utilizar la sentencia 
+Rollback de la manera adecuada.*/
+
+/*Sin usar rollback, o utilizandolo mal?
+
+/*8 - Habilitar el modo de transacciones implícitas, ejecutar el siguiente código
+insert into equipo (nombre_equipo)
+values ('Atenas de cordoba');
+cerrar el SQL y verificar que sucede.*/
+
+/*Autocommit on? debería comitear luego del insert, cierre o no el programa*/
+
